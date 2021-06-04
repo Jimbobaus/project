7bd1897dc3c67055b3792785ea30d1ad8874f5ae
Peter Morian <59523646+petermorian@users.noreply.github.com>
2021-06-04 18:08:37 +1000

    Add files via upload

--- src/time series code.R
+++ src/time series code.R
@@ -18,8 +18,9 @@ train_y = setDT(read.csv(train_y_loc, header=T))
 test_y = setDT(read.csv(test_y_loc, header=T))
 all = setDT(rbind(train_y,test_y))[order(DATETIME)] %>% mutate(DATETIME=ymd_hms(DATETIME))
 all_y = data.frame(all$TOTALDEMAND)
-train_y = subset(all_y, end=0.8*length(all_y))   # first 80%
-test_y= subset(all_y, start=0.8*length(all_y)+1) # last 20%
+train_y = head(all_y, 0.8*nrow(all_y))   # first 80%
+test_y= tail(all_y, 0.2*nrow(all_y)) # last 20%
+sum(all$TOTALDEMAND) == sum(train_y) + sum(test_y) # TRUE
 
 # --- convert to time series format
 findfrequency(train_y) # 48 (number of 30min intervals in a day)
@@ -28,27 +29,27 @@ train_y_ts =ts(train_y, frequency = 48)
 # --- summary stats of training data
 (train_y_ts_summary=describe(train_y_ts))
 # vars      n    mean      sd  median trimmed     mad     min      max   range skew kurtosis   se
-# X1    1 161185 7970.52 1240.24 7925.72 7914.81 1246.72 5074.63 13985.87 8911.24 0.47     0.21 3.09
+# X1    1 128948 8024.58 1239.18 8011.53 7978.95 1243.71 5074.63 13985.87 8911.24  0.4     0.19 3.45
 
 # --- model
-(model_arima = auto.arima(train_y_ts, seasonal = F)) # arima(3,1,2)
+(model_arima = auto.arima(train_y_ts, seasonal = F)) # arima(4,1,2)
 # Series: train_y_ts 
-# ARIMA(3,1,2) 
+# ARIMA(4,1,2) 
 # 
 # Coefficients:
-#   ar1     ar2      ar3      ma1      ma2
-# 1.3357  0.0311  -0.4233  -0.5668  -0.4143
-# s.e.  0.0127  0.0229   0.0107   0.0129   0.0128
+#   ar1     ar2      ar3      ar4      ma1      ma2
+# 0.8243  0.8931  -0.7242  -0.0681  -0.0446  -0.9328
+# s.e.  0.0040  0.0060   0.0042   0.0029   0.0029   0.0029
 # 
-# sigma^2 estimated as 12944:  log likelihood=-991782.5
-# AIC=1983577   AICc=1983577   BIC=1983637
+# sigma^2 estimated as 13143:  log likelihood=-794412.1
+# AIC=1588838   AICc=1588838   BIC=1588907
 
 # --- accuracy
 test_y_ts =ts(test_y, frequency = 48)
 model_results <- Arima(test_y_ts, model=model_arima)
-accuracy(model_results) # 113.7675 RMSE
-# ME     RMSE      MAE         MPE   MAPE      MASE       ACF1
-# Training set 0.006098198 113.7675 83.90262 -0.01790531 1.042687 0.1888699 0.02374432
+accuracy(model_results) # 110.615 RMSE
+# ME    RMSE     MAE          MPE     MAPE      MASE       ACF1
+# Training set -0.1000962 110.615 82.1002 -0.009748878 1.039758 0.1801267 0.05788134
 
 
 
